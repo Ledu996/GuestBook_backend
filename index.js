2738aa25e33c03c8923725e19d5c8f3bb4c1e047
Ledu996 <dusan.dorcol@hotmail.com>
2023-05-25 14:34:04 +0200

    feat(app): Initialized app and installed required packages

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Ready to create small app");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+const messageRoutes = require('./components/message/messageRouter');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/message', messageRoutes);
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000");
+});
